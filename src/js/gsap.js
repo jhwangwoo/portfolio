cbef4a1c134d0bc9bb9414a2f4e94f985aefd4be
jhwangwoo <125618964+jhwangwoo@users.noreply.github.com>
2023-06-27 20:52:57 +0900

    포트폴리오

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -14,53 +14,42 @@ requestAnimationFrame(raf);
 const arrowTimeline = gsap.timeline({ repeat: -1 });
 
 arrowTimeline
-// {y: 12, ease: "power1.inOut", repeat: -1, yoyo: true}
-  .to('.scrolldesc__arrow', { y: '-20%', opacity: 1, duration: 1 })
-  .to('.scrolldesc__arrow', { y: '50%', opacity: 0, duration: 1 });
+    // {y: 12, ease: "power1.inOut", repeat: -1, yoyo: true}
+    .to('.scrolldesc__arrow', { y: '-20%', opacity: 1, duration: 1 })
+    .to('.scrolldesc__arrow', { y: '50%', opacity: 0, duration: 1 });
 
 // 페이지 로드 시 애니메이션 시작
 arrowTimeline.play();
 
 const ani1 = gsap.timeline();
-ani1.fromTo(".portfolio__title__line", {width: "0.18vw", height: "0" },{width: "0.18vw", height: "10.3vw" })
-    .from(".portfolio__title h2 .title.t1", {duration: 5, xPercent:150}, "text")
-    .from(".portfolio__title h2 .title.t2", {xPercent:-150,duration: 5}, "text")
-    .to(".portfolio__title__line", {width: "0.18vw", height: "0" })
-    .to(".portfolio__title__line", {width: "0.18vw", height: "0" })
-    .to(".portfolio__title__line", {width: "0", height: "0" })
-    .to(".portfolio__title__line", {width: "0", height: "0" })
-    .to(".portfolio__title__line", {width: "0", height: "0" })
-
+ani1.fromTo('.portfolio__title__line', { width: '0.18vw', height: '0' }, { width: '0.18vw', height: '10.3vw' })
+    .from('.portfolio__title h2 .title.t1', { xPercent: 150, duration: 2 }, 'text')
+    .from('.portfolio__title h2 .title.t2', { xPercent: -150, duration: 2 }, 'text')
+    .to('.portfolio__title__line', { width: '0.18vw', height: '0' })
+    .to('.portfolio__title__line', { width: '0.18vw', height: '0' })
+    .to('.portfolio__title__line', { width: '0', height: '0' })
+    .to('.portfolio__title__line', { width: '0', height: '0' });
 
 ScrollTrigger.create({
     animation: ani1,
-    trigger: ".portfolio__desc.t0",
-    start: "top -31%",
-    end: "+=4500",
+    trigger: '.portfolio__desc.t0',
+    start: 'top -31%',
+    end: '+=4500vh',
     scrub: true,
     // pin: true,
     // markers: true,
     // anticipatePin: 1
 });
-
 const ani2 = gsap.timeline();
-ani2.to('.portfolio__desc', {
-    scale: 0.65,
-    duration: 5,
-    ease: "circ.inOut",
-    easeParams: [1, 0.3] // easeParams를 조정하여 효과를 변경
-  }, "a");
-ani2.to('.title.t1', { yPercent:15, duration:5 ,   ease: "circ.inOut",
-easeParams: [1, 0.3] // easeParams를 조정하여 효과를 변경
-}, "a");
-ani2.to('.title.t2', { yPercent:-15, duration:5 ,   ease: "circ.inOut",
-easeParams: [1, 0.3] // easeParams를 조정하여 효과를 변경
-}, "a");
+ani2.to('.portfolio__desc', { scale: 0.65, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
+ani2.to('.title.t1', { yPercent: 15, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
+ani2.to('.title.t2', { yPercent: -15, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
+ani2.to('.scrolldesc', { opacity: 0, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
 ScrollTrigger.create({
     animation: ani2,
     trigger: '.portfolio__desc.t0',
-    start: 'bottom -300%',
-    end: '+=8000',
+    start: '4500vh',
+    end: '27500vh',
     scrub: true,
     // pin: true,
     // anticipatePin: 1,
@@ -68,12 +57,33 @@ ScrollTrigger.create({
 });
 
 const ani3 = gsap.timeline();
-ani3.fromTo('.portfolio__item.t1 ul li', { xPercent: 500,ease: "power4.out"},{ xPercent: -600,ease: "power4.out"})
+ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -600, delay: 0.24, duration: 0.4 }, 'a');
 ScrollTrigger.create({
     animation: ani3,
-    trigger: '.portfolio__desc.t1',
-    start: 'bottom -700%',
-    end: '+=15000',
+    trigger: '.portfolio__desc.t0',
+    start: '12500vh',
+    end: '17500vh',
+    scrub: true,
+    // pin: true,
+    // anticipatePin: 1,
+    // markers: true,
+});
+
+const ani4 = gsap.timeline();
+ani2.fromTo('.const1', { yPercent: -105 }, { yPercent: 0, ease: 'expo.inOut', easeParams: [0.1, 0.5] }, 'b');
+ani2.fromTo('.const2', { yPercent: 105 }, { yPercent: 0, ease: 'expo.inOut', easeParams: [0.1, 0.5] }, 'b');
+ani2.to(
+    '.portfolio__desc',
+    { scale: 0.45, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.05 },
+    'b',
+);
+ani2.to('.title.t1', { yPercent: 55, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.4, delay: 0.1 }, 'b');
+ani2.to('.title.t2', { yPercent: -55, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.4, delay: 0.1 }, 'b');
+ScrollTrigger.create({
+    animation: ani4,
+    trigger: '.portfolio__desc.t0',
+    start: '17500vh',
+    end: '28150vh',
     scrub: true,
     // pin: true,
     // anticipatePin: 1,
