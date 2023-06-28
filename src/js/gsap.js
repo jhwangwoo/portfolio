6eeb8f7ef3f1582086f45b25d9cf93a611927e40
jhwangwoo <125618964+jhwangwoo@users.noreply.github.com>
2023-06-28 18:49:20 +0900

    포트폴리오

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -40,14 +40,14 @@ ScrollTrigger.create({
     markers: false,
 });
 const ani2 = gsap.timeline();
-ani2.to('.portfolio__desc', { scale: 0.65, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
+ani2.to('.portfolio__desc', { scale: 0.65, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.45 }, 'a');
 ani2.to('.title.t1', { yPercent: 15, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
 ani2.to('.title.t2', { yPercent: -15, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
 ani2.to('.scrolldesc', { opacity: 0, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
 ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -609, delay: 0.24, duration: 0.4 }, 'a');
 
 ani2.fromTo('.const1', { yPercent: -105 }, { yPercent: 0, ease: 'expo.inOut', easeParams: [0.1, 0.2] }, 'b');
-ani2.fromTo('.const2', { yPercent: 105 }, { yPercent: 0, ease: 'expo.inOut', easeParams: [0.1, 0.2] }, 'b');
+ani2.fromTo('.const2', { yPercent: 118 }, { yPercent: 0, ease: 'expo.inOut', easeParams: [0.1, 0.2] }, 'b');
 ani2.to('.portfolio__desc',{ scale: 0.45, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.07 },'b',);
 ani2.to('.title.t1', { yPercent: 55, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.4, delay: 0.1 }, 'b');
 ani2.to('.title.t2', { yPercent: -55, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.4, delay: 0.1 }, 'b');
