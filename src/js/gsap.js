7bbf8b97e359932a9f5b49ed1baca2141acffd70
jhwangwoo <125618964+jhwangwoo@users.noreply.github.com>
2023-06-30 17:55:04 +0900

    포트폴리오

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -2,7 +2,7 @@
 const lenis = new Lenis();
 
 lenis.on('scroll', e => {
-    console.log(e);
+    // console.log(e);
 });
 
 function raf(time) {
@@ -22,6 +22,7 @@ arrowTimeline
 // 페이지 로드 시 애니메이션 시작
 arrowTimeline.play();
 
+
 const ani1 = gsap.timeline();
 ani1.fromTo('.portfolio__title__line', { width: '0.18vw', height: '0' }, { width: '0.18vw', height: '10.3vw' })
     .from('.portfolio__title h2 .title.t1', { xPercent: 150, duration: 2 }, 'text')
@@ -40,26 +41,26 @@ ScrollTrigger.create({
     markers: false,
 });
 const ani2 = gsap.timeline();
-ani2.to('.portfolio__desc', { scale: 0.65, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.45 }, 'a');
-ani2.to('.title.t1', { yPercent: 15, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
-ani2.to('.title.t2', { yPercent: -15, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
-ani2.to('.scrolldesc', { opacity: 0, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.5 }, 'a');
-ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -609, delay: 0.24, duration: 0.8 }, 'a');
+ani2.to('.portfolio__desc', { scale: 0.65, ease: 'expo.inOut', duration: 0.45 }, 'a');
+ani2.to('.title.t1', { yPercent: 15, ease: 'expo.inOut', duration: 0.5 }, 'a');
+ani2.to('.title.t2', { yPercent: -15, ease: 'expo.inOut', duration: 0.5 }, 'a');
+ani2.to('.scrolldesc', { opacity: 0, ease: 'expo.inOut', duration: 0.5 }, 'a');
+ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -670, delay: 0.24, duration: 0.5 }, 'a');
 
-ani2.fromTo('.const1', { yPercent: -105 }, { yPercent: 0, ease: 'expo.inOut', easeParams: [0.1, 0.2] }, 'b');
-ani2.fromTo('.const2', { yPercent: 118 }, { yPercent: 0, ease: 'expo.inOut', easeParams: [0.1, 0.2] }, 'b');
-ani2.to('.portfolio__desc',{ scale: 0.45, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.07 },'b',);
-ani2.to('.title.t1', { yPercent: 55, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.4, delay: 0.1 }, 'b');
-ani2.to('.title.t2', { yPercent: -55, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.4, delay: 0.1 }, 'b');
+ani2.fromTo('.const1', { yPercent: -105 }, { yPercent: 0, ease: 'expo.inOut' }, 'b');
+ani2.fromTo('.const2', { yPercent: 118 }, { yPercent: 0, ease: 'expo.inOut' }, 'b');
+ani2.to('.portfolio__desc',{ scale: 0.45, ease: 'expo.inOut', duration: 0.35, delay: 0.07 },'b',);
+ani2.to('.title.t1', { yPercent: 55, ease: 'expo.inOut', duration: 0.4, delay: 0.1 }, 'b');
+ani2.to('.title.t2', { yPercent: -55, ease: 'expo.inOut', duration: 0.4, delay: 0.1 }, 'b');
 
 ani2.to('.title.t1', { opacity: 0 }, 'c');
 ani2.to('.title.t2', { opacity: 0 }, 'c');
 ani2.to('.portfolio__item.t2 ul li', { yPercent: -702, duration: 1 }, 'c');
-ani2.to('.portfolio__desc',{ scale: 0.31, ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.5 },'c',);
-ani2.from('.my__desc',{ xPercent:-200 ,ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.5 },'c',);
-ani2.from('.my__desc2',{ xPercent:-400 ,ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.5 },'c',);
-ani2.from('.my__img',{ xPercent:-200 ,ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.5 },'c',);
-ani2.from('.my__link',{ xPercent:200 ,ease: 'expo.inOut', easeParams: [0.1, 1], duration: 0.35, delay: 0.5 },'c',);
+ani2.to('.portfolio__desc',{ scale: 0.31, ease: 'expo.inOut', duration: 0.35, delay: 0.5 },'c',);
+ani2.from('.my__desc',{ xPercent:-200 ,ease: 'expo.inOut', duration: 0.35, delay: 0.5 },'c',);
+ani2.from('.my__desc2',{ xPercent:-400 ,ease: 'expo.inOut', duration: 0.35, delay: 0.5 },'c',);
+ani2.from('.my__img',{ xPercent:-200 ,ease: 'expo.inOut', duration: 0.35, delay: 0.5 },'c',);
+ani2.from('.my__link',{ xPercent:200 ,ease: 'expo.inOut', duration: 0.35, delay: 0.5 },'c',);
 
 ScrollTrigger.create({
     animation: ani2,
