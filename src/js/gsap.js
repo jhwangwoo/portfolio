1e75d815e2d1fd954b5a45fb499ab8d3af768a22
jhwangwoo <125618964+jhwangwoo@users.noreply.github.com>
2023-07-06 12:08:14 +0900

    포트폴리오

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -38,13 +38,20 @@ ScrollTrigger.create({
     scrub: true,
     markers: false,
 });
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+console.log(width);
 const ani2 = gsap.timeline();
 ani2.to('.portfolio__desc', { scale: 0.65, ease: 'expo.inOut', duration: 0.45 }, 'a');
 ani2.to('.title.t1', { yPercent: 15, ease: 'expo.inOut', duration: 0.5 }, 'a');
 ani2.to('.title.t2', { yPercent: -15, ease: 'expo.inOut', duration: 0.5 }, 'a');
 ani2.to('.scrolldesc', { opacity: 0, ease: 'expo.inOut', duration: 0.5 }, 'a');
-ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -560, delay: 0.24, duration: 0.5 }, 'a');
-
+if (width <= 530) {
+    ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -670, delay: 0.24, duration: 0.5 }, 'a');
+} else if (width <= 820) {
+    ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -600, delay: 0.24, duration: 0.5 }, 'a');
+} else {
+    ani2.fromTo('.portfolio__item.t1 ul li', { xPercent: 480 }, { xPercent: -560, delay: 0.24, duration: 0.5 }, 'a');
+}
 ani2.fromTo('.const1', { yPercent: -105 }, { yPercent: 0, ease: 'expo.inOut' }, 'b');
 ani2.fromTo('.const2', { yPercent: 118 }, { yPercent: 0, ease: 'expo.inOut' }, 'b');
 ani2.to('.portfolio__desc', { scale: 0.45, ease: 'expo.inOut', duration: 0.35, delay: 0.07 }, 'b');
@@ -53,7 +60,12 @@ ani2.to('.title.t2', { yPercent: -55, ease: 'expo.inOut', duration: 0.4, delay: 
 
 ani2.to('.title.t1', { opacity: 0 }, 'c');
 ani2.to('.title.t2', { opacity: 0 }, 'c');
-ani2.to('.portfolio__item.t2 ul li', { yPercent: -550, duration: 1 }, 'c');
+if (width <= 630) {
+    ani2.to('.portfolio__item.t2 ul li', { yPercent: -600, duration: 1 }, 'c');
+} else {
+    ani2.to('.portfolio__item.t2 ul li', { yPercent: -550, duration: 1 }, 'c');
+}
+
 ani2.to('.portfolio__desc', { scale: 0.31, ease: 'expo.inOut', duration: 0.35, delay: 0.5 }, 'c');
 
 ani2.from('.my__desc', { xPercent: -200, ease: 'expo.inOut', duration: 0.35, delay: 0.5 }, 'c');
@@ -65,7 +77,8 @@ ScrollTrigger.create({
     animation: ani2,
     trigger: '.portfolio__desc.t0',
     start: '4500vh',
-    end: '27500vh',
+    // end: '27500vh',
+    end: '22000vh',
     scrub: true,
     // pin: true,
     // anticipatePin: 1,
